9d956dd9012ce47c5d28174fcd98f972efb252db
Qenan.Rosine <qenan.rosine@gmail.com>
2025-02-03 23:00:58 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath: isProd ? '/Portfolio-dev' : '',
+  assetPrefix: isProd ? '/Portfolio-dev/' : '',
   swcMinify: true,
   compress: true,
   productionBrowserSourceMaps: false,
